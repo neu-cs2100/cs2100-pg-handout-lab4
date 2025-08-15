e9b15625471650dedfe93901bfa4d48ddfea2783
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2025-08-15 11:07:01 -0400

    Change stack to queue

--- q1.py
+++ q1.py
@@ -1,12 +1,12 @@
 
 """
 This question has no starter code.
-Write a generic class called Stack, which, like List, can store any type of data.
+Write a generic class called Queue, which, like List, can store any type of data.
 
-The class Stack should have the following methods:
+The class Queue should have the following methods:
 
-push(item: T) -> None: Add an item to the top of the stack (front of the list)
-pop() -> T | None: Remove and return the top item (return None if empty)
-peek() -> T | None: Return the top item without removing it (return None if empty)
-is_empty() -> bool: Check if the stack is empty
+enqueue(item: T) -> None: Add an item to the back of the queue (back of the list)
+dequeue() -> T | None: Remove and return the front item (return None if empty)
+peek() -> T | None: Return the front item without removing it (return None if empty)
+is_empty() -> bool: Check if the queue is empty
 """
